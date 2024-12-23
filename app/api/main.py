d9f145a1114b9196a2a8ed091a89bf95b1893bba
0x5c0f <mail@0x5c0f.cc>
2024-12-23 14:26:20 +0800

    refactor: 关闭一些不必要的功能

--- app/api/main.py
+++ app/api/main.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-api_router.include_router(items.router, prefix="/items", tags=["items"])
+# api_router.include_router(items.router, prefix="/items", tags=["items"])
 
 
 from app.api.routes.rz import performance_data, performance_tracking_config
