8d3d25bbddb66c4204521a498b93f76c033770c3
0x5c0f <mail@0x5c0f.cc>
2025-08-06 15:40:39 +0800

    perf: 添加 resolve_domain mcp 支持

--- app/rz/subsystem/main.py
+++ app/rz/subsystem/main.py
@@ -11,7 +11,7 @@ def register_mcp_server(app: FastAPI):
         app,
         name=f"{settings.PROJECT_NAME} API MCP",
         description=f"MCP server for the {settings.PROJECT_NAME} API",
-        include_operations=["list_mcp_operations"],
+        include_operations=["list_mcp_operations", "resolve_domain"],
     )
 
     mcp.mount_http(mount_path="/mcp/info")
--- app/rz/subsystem/mcp/main.py
+++ app/rz/subsystem/mcp/main.py
@@ -30,6 +30,10 @@ async def list_mcp_operations():
                 "name": "list_mcp",
                 "description": "查询当前 MCP 可用的接口有哪些.",
             },
+            {
+                "name": "resolve_domain",
+                "description": "解析域名并获取其 IP 地址信息",
+            }
         ]
     }
     
