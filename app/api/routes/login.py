d746dee11a66649b0d50f39a690107e3b29bbbb6
0x5c0f <mail@0x5c0f.cc>
2025-08-07 15:46:25 +0800

    refactor: 关闭邮件恢复密码功能

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -50,29 +50,29 @@ def test_token(current_user: CurrentUser) -> Any:
     """
     return current_user
 
+# # refactor: 关闭邮件恢复密码功能
+# @router.post("/password-recovery/{email}")
+# def recover_password(email: str, session: SessionDep) -> Message:
+#     """
+#     Password Recovery
+#     """
+#     user = crud.get_user_by_email(session=session, email=email)
 
-@router.post("/password-recovery/{email}")
-def recover_password(email: str, session: SessionDep) -> Message:
-    """
-    Password Recovery
-    """
-    user = crud.get_user_by_email(session=session, email=email)
-
-    if not user:
-        raise HTTPException(
-            status_code=404,
-            detail="The user with this email does not exist in the system.",
-        )
-    password_reset_token = generate_password_reset_token(email=email)
-    email_data = generate_reset_password_email(
-        email_to=user.email, email=email, token=password_reset_token
-    )
-    send_email(
-        email_to=user.email,
-        subject=email_data.subject,
-        html_content=email_data.html_content,
-    )
-    return Message(message="Password recovery email sent")
+#     if not user:
+#         raise HTTPException(
+#             status_code=404,
+#             detail="The user with this email does not exist in the system.",
+#         )
+#     password_reset_token = generate_password_reset_token(email=email)
+#     email_data = generate_reset_password_email(
+#         email_to=user.email, email=email, token=password_reset_token
+#     )
+#     send_email(
+#         email_to=user.email,
+#         subject=email_data.subject,
+#         html_content=email_data.html_content,
+#     )
+#     return Message(message="Password recovery email sent")
 
 
 @router.post("/reset-password/")
